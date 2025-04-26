26137c4f59436e1ff558723074fa11c395be437e
Shlok Jadhav <shloksjadhav2005@gmail.com>
2025-04-26 23:08:21 +0530

    fixed error

--- controllers/listings.js
+++ controllers/listings.js
@@ -18,19 +18,22 @@ module.exports.showListing = async (req, res) => {
 };
 
 module.exports.createListing = async (req, res, next) => {
-  // if (!req.body.listing.image.url) {
-  //   delete req.body.listing.image.url; // Let mongoose use default
-  // }
+  try {
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
 
-  let url = req.file.path;
-  let filename = req.file.filename;
+    if (req.file) {
+      let url = req.file.path;
+      let filename = req.file.filename;
+      newListing.image = { url, filename };
+    }
 
-  const newListing = new Listing(req.body.listing);
-  newListing.owner = req.user._id;
-  newListing.image = { url, filename };
-  await newListing.save();
-  req.flash("success", "New Listing Created!");
-  res.redirect("/listings");
+    await newListing.save();
+    req.flash("success", "New Listing Created!");
+    res.redirect("/listings");
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports.editListing = async (req, res) => {
